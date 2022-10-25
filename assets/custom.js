6bab503003a2e6dfc647d0e92bf9e07754b1d227
somsadkh <somsad.kh@gmail.com>
2022-10-25 02:36:55 -0400

    1) Delete Avada EM, Affirm, Stamped, Ultimate Countdown 2) Icons optimization on product page

--- assets/custom.js
+++ assets/custom.js
@@ -2,14 +2,17 @@
 // button.setAttribute("aria-disabled", "true")
 
 
-const add_to_cart = document.querySelector("button.btn.product-form__cart-submit.btn--secondary-accent");
+// const add_to_cart = document.querySelector("button.btn.product-form__cart-submit.btn--secondary-accent");
 
-let myVar = setInterval(myTimer ,100);
-function myTimer() {
-  if (document.getElementsByTagName("script").length > 105){
-    console.log(document.getElementsByTagName("script"));
-    console.log("add to cart loading...")
-    add_to_cart.removeAttribute('aria-disabled');
-    clearInterval(myVar);
-  }
-}
+// let myVar = setInterval(myTimer ,100);
+// var num = 0
+// function myTimer() {
+//   num++;
+//   console.log(num)
+//   if (document.getElementsByTagName("script").length > 105){
+//     console.log(document.getElementsByTagName("script"));
+//     console.log("add to cart loading...")
+//     add_to_cart.removeAttribute('aria-disabled');
+//     clearInterval(myVar);
+//   }
+// }
